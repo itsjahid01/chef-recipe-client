c70903f938cc5cdf518ad857f1a57b8ca519435e
itsjahid01 <jahidnoman0@gmail.com>
2023-05-04 22:03:15 +0600

    banner added

--- src/Pages/Root.jsx
+++ src/Pages/Root.jsx
@@ -7,7 +7,7 @@ const Root = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <div className="min-h[calc(100vh-64px)]">
+      <div className="min-h[calc(100vh-(257+64)px)]">
         <Outlet></Outlet>
       </div>
       <Footer></Footer>
